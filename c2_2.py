e6a79095460dfc665b6e3d331d36aeab84b522ac
Xiangyz <5555wyhhhgxx@163.com>
2017-11-01 18:01:58 +0800

    tuple namedtuple

--- c2_2.py
+++ c2_2.py
@@ -8,3 +8,8 @@ print(t)
 a = array.array('I', (ord(symbol) for symbol in symbols))
 
 print(a)
+
+colors = ['black', 'white']
+sizes = ['S', 'M', 'L']
+for tshirt in ('%s %s' % (c, s) for c in colors for s in sizes):
+    print(tshirt)
