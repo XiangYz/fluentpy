6c53fbf88f0e4378c60bc1fe92e23d1094738573
Xiangyz <5555wyhhhgxx@163.com>
2017-11-17 17:52:57 +0800

    20171117

--- c3_10.py
+++ c3_10.py
@@ -1,9 +1,16 @@
+from dis import dis
+
+
 s1 = set(['spam', 'spam', 'eggs'])
 print(s1)
 
-s2 = set(['spam', 'abc'])
+s2 = set(('spam', 'abc'))
 print(s2)
 
 print(s1 & s2)
 print(s1 | s2)
 print(s1 - s2)
+
+
+print(dis('{1}'))
+print(dis('set([1])'))
